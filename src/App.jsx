231d8a9dc4a77173a04120db77469a349ecaa667
Tanvir Memon <tanvirmemon48@gmail.com>
2024-09-18 22:38:22 +0530

    tester

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+      
+        <h1>Hello 👋 </h1>
+        <h1> I am Tannu 👩‍💻 </h1>
+     
+        <h1>
+         Frontend Developer 🔮
+</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
